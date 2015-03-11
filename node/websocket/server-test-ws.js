c990c020c884a051b987e92630346c57bab4380a
Chad Berkley <cberkley@flightstats.com>
2015-03-10 20:35:30 -0700

    adding websocket transaction test harness

---
+++ node/websocket/server-test-ws.js
@@ -0,0 +1,28 @@
+var WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({ port: 3001 });
+console.log("WebSocketServer created on port 3001");
+
+wss.on('connection', function connection(ws) {
+  console.log("connection made...");
+
+  ws.on('message', function(message) {
+    console.log("message received on server ", message);
+    //do something then send an ack
+    var msgJSON = JSON.parse(message);
+    var envelope = {"id":msgJSON.id, "message":"ok"};
+    if(ws.readyState == ws.OPEN) {
+      ws.send(JSON.stringify(envelope), function(err){
+        console.log(envelope, " sent");
+      });
+    }
+  });
+
+  ws.on('close', function(){
+    console.log("close");
+    ws.terminate();
+  });
+});
+
+wss.on('error', function(err){
+
+});
