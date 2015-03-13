721f41d524aca89f70bcb68ffd2d917157ff4cea
Chad Berkley <cberkley@gmail.com>
2015-03-12 22:47:10 -0700

    more websocket testing and debugging

--- node/websocket/server-test-ws.js
+++ node/websocket/server-test-ws.js
@@ -1,4 +1,6 @@
-var WebSocketServer = require('ws').Server
+  var count = 0;
+
+  var WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({ port: 3001 });
 console.log("WebSocketServer created on port 3001");
 
@@ -7,20 +9,27 @@ wss.on('connection', function connection(ws) {
 
   ws.on('message', function(message) {
     console.log("message received on server ", message);
-    //do something then send an ack
-    var msgJSON = JSON.parse(message);
-    var envelope = {"id":msgJSON.id, "message":"ok"};
-    if(ws.readyState == ws.OPEN) {
-      ws.send(JSON.stringify(envelope), function(err){
-        console.log(envelope, " sent");
-      });
-    }
+    // //do something then send an ack
+    // var msgJSON = JSON.parse(message);
+    // var envelope = {"id":msgJSON.id, "message":"ok"};
+    // if(ws.readyState == ws.OPEN) {
+    //   ws.send(JSON.stringify(envelope), function(err){
+    //     console.log(envelope, " sent");
+    //   });
+    // }
+
+    
   });
 
   ws.on('close', function(){
     console.log("close");
     ws.terminate();
   });
+
+  setInterval(function(){
+      ws.send("ok1 " + count++, function(){});
+  }, 50); 
+
 });
 
 wss.on('error', function(err){
