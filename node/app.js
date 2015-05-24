89f079295beaf24e2aaeb3769780a40b264218d9
Chad Berkley <cberkley@flightstats.com>
2015-05-24 10:45:01 -0700

    refactoring to allow this app to have more than just lighting routes

--- node/app.js
+++ node/app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var spark = require('./routes/spark');
+var lights = require('./routes/lights');
 var http = require('http');
 var path = require('path');
 var nconf = require('nconf');
@@ -13,7 +13,7 @@ nconf.argv()
      .env()
      .file({file: 'config.json'});
 
-spark.setConfig(nconf);
+lights.setConfig(nconf);
 
 app.set('port', nconf.get("port") || process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -26,9 +26,9 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', spark.index);
-app.get('/house/run/action', spark.action);
-app.get('/house/params/:coreId', spark.params); //get the current param state for the given coreId;
+app.get('/', lights.index);
+app.get('/house/run/action', lights.action);
+app.get('/house/params/:coreId', lights.params); //get the current param state for the given coreId;
 
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
