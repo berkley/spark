b352bae6f599078c0e92bd45cbb2c160eebcbced
Chad Berkley <cberkley@flightstats.com>
2015-06-04 22:35:53 -0700

    reversing changes to websocket code

--- firmware/websocket-neopixel/build.sh
+++ firmware/websocket-neopixel/build.sh
@@ -1,5 +1,3 @@
 rm firmware*
-particle compile spark-ws-pixels.ino neopixel.h neopixel.cpp Spark-Websockets.cpp Spark-Websockets.h 
-particle flash --usb firmware*.bin
-
-
+spark compile spark-ws-pixels.ino neopixel.h neopixel.cpp Spark-Websockets.cpp Spark-Websockets.h 
+spark flash --usb firmware*.bin
