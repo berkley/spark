b7e69f9f5f43775f027f2844b21bd186b05ec63c
Chad Berkley <cberkley@flightstats.com>
2015-09-22 10:11:55 -0700

    adding beer sensor sketch

---
+++ firmware/beer/beersensor.ino
@@ -0,0 +1,146 @@
+// This #include statement was automatically added by the Particle IDE.
+#include "neopixel/neopixel.h"
+
+#define PIXEL_PIN D2
+#define PIXEL_TYPE WS2812B
+#define PIXEL_COUNT 5
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
+
+int ledPin = D7;
+
+int pin0 = A0;
+int pin1 = D1;
+int pin2 = A2;
+int pin3 = D3;
+int pin4 = A4;
+int pin5 = D5;
+int pin6 = D6;
+
+int pin0Value = 0;
+int pin1Value = 0;
+int pin2Value = 0; 
+int pin3Value = 0;
+int pin4Value = 0;
+int pin5Value = 0;
+int pin6Value = 0;
+
+int pulseCount = 0;
+int pulseSerial = 0;
+int currentSecond = 1;
+int previousSecond = 0;
+
+const char* pin0Name = "pin0Value";
+const char* pin1Name = "pin1Value";
+const char* pin2Name = "pin2Value";
+const char* pin3Name = "pin3Value";
+const char* pin4Name = "pin4Value";
+const char* pin5Name = "pin5Value";
+const char* pin6Name = "pin6Value";
+
+void setup()
+{
+  Serial.begin(9600);
+  strip.begin();
+  strip.show();
+  
+  pinMode(pin0, INPUT_PULLDOWN);
+  pinMode(pin1, INPUT_PULLDOWN);
+  pinMode(pin2, INPUT_PULLDOWN);
+  pinMode(pin3, INPUT_PULLDOWN);
+//   pinMode(pin4, INPUT_PULLDOWN);
+  pinMode(pin4, INPUT_PULLDOWN);
+  pinMode(pin5, INPUT_PULLDOWN);
+  pinMode(pin6, INPUT_PULLDOWN);
+//   pinMode(pin6, INPUT);
+
+  pinMode(ledPin, OUTPUT);
+
+  Spark.variable(pin0Name, &pin0Value, INT);
+  Spark.variable(pin1Name, &pin1Value, INT);
+  Spark.variable(pin2Name, &pin2Value, INT);
+  Spark.variable(pin3Name, &pin3Value, INT);
+  Spark.variable(pin4Name, &pin4Value, INT);
+  Spark.variable(pin5Name, &pin5Value, INT);
+  Spark.variable(pin6Name, &pin6Value, INT);
+  Spark.variable("pulseCount", &pulseCount, INT);
+  Spark.variable("pulseSerial", &pulseSerial, INT);
+}
+
+void loop()
+{
+//   pinVal(pin0, &pin0Value, pin0Name);
+//   pinVal(pin1, &pin1Value, pin1Name);
+//   analogVal(pin2, &pin2Value, pin2Name);
+//   pinVal(pin3, &pin3Value, pin3Name);
+//   pinVal(pin4, &pin4Value, pin4Name);
+//   analogVal(pin4, &pin4Value, pin4Name);
+//   pinVal(pin5, &pin5Value, pin5Name);
+  pinVal(pin6, &pin6Value, pin6Name);
+    //  analogVal(pin6, &pin6Value, pin6Name);
+//   delay(100);
+}
+
+void analogVal(int pin, int* pinValue, String pinName)
+{
+    
+    *pinValue = analogRead(pin);  // read the input pin
+    // Serial.println(pinName + ":" + String(*pinValue));
+    digitalWrite(ledPin, *pinValue/16);
+}
+
+void pinVal(int pin, int* pinValue, String pinName)
+{
+  if(digitalRead(pin) == HIGH)
+  {
+    if(*pinValue == 0)
+    {
+    //   Spark.publish(pinName,"HIGH", 60, PRIVATE);
+      *pinValue = 1; //door is closed
+      digitalWrite(ledPin, LOW);
+    }
+  }
+  else
+  {
+    if(*pinValue == 1)
+    {
+    //   Spark.publish(pinName,"LOW", 60, PRIVATE);
+      *pinValue = 0; //door is open
+      digitalWrite(ledPin, HIGH);
+      pulseCount++;
+      pulseSerial++;
+    }
+  }
+  
+  
+  currentSecond = Time.second();
+//   Serial.println("CurrentSecond: " + String(currentSecond));
+  if(currentSecond != previousSecond)
+  {
+      setPixels(pulseCount);
+      pulseCount = 0;
+      previousSecond = currentSecond;
+  }
+  
+//   Serial.println(pinName + ":" + String(*pinValue));
+}
+
+void setPixels(int pulseCount)
+{
+    strip.setPixelColor(0, strip.Color(0, 0, 0));
+    strip.setPixelColor(1, strip.Color(0, 0, 0));
+    strip.setPixelColor(2, strip.Color(0, 0, 0));
+    strip.setPixelColor(3, strip.Color(0, 0, 0));
+    strip.setPixelColor(4, strip.Color(0, 0, 0));
+    Serial.println("PulseCount: " + String(pulseCount));
+    if(pulseCount > 0)
+    strip.setPixelColor(0, strip.Color(0, 255, 0));
+    if(pulseCount > 100)
+    strip.setPixelColor(1, strip.Color(0, 255, 0));
+    if(pulseCount > 200)
+    strip.setPixelColor(2, strip.Color(255, 165, 0));
+    if(pulseCount > 300)
+    strip.setPixelColor(3, strip.Color(255, 165, 0));
+    if(pulseCount > 400)
+    strip.setPixelColor(4, strip.Color(255, 0, 0));
+    strip.show();
+}
