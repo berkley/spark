719cbe69c7ba64aba918fa4d61be8e2d88828437
Chad Berkley <cberkley@gmail.com>
2015-10-05 21:40:40 -0700

    working beer app

--- firmware/beer/beersensor.ino
+++ firmware/beer/beersensor.ino
@@ -7,10 +7,11 @@ TODO
   * API endpoints:
     x reset
     x get data
+    x set calibration
     * set tap name
     * animation (beer) type
-
 */
+
 // This #include statement was automatically added by the Particle IDE.
 #include "neopixel/neopixel.h"
 
@@ -19,7 +20,7 @@ TODO
 #define EEPROM_ADDR_PERCENT_FULL 3
 #define PIXEL_PIN D2
 #define PIXEL_TYPE WS2812B
-#define PIXEL_COUNT 5
+#define PIXEL_COUNT 7
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
 
@@ -42,9 +43,11 @@ int pin5Value = 0;
 int pin6Value = 0;
 
 int instantPulseCount = 0;
-int pulseCount = 0;
-int maxPulseCount = 10000; //bogus init value
-int percentFull = 0;
+int previousPulseCount = 0;
+bool flowing = false;
+int pulseCount = 200;
+int maxPulseCount = 200; //bogus init value //real value for chad's fridge left tap: 45 pulses / qt = 900 pulses for a 5 gal keg
+int percentFull = 100;
 int currentSecond = 1;
 int previousSecond = 0;
 int animationStep = 0;
@@ -64,12 +67,13 @@ void setup()
 {
   Serial.begin(9600);
   strip.begin();
+  strip.setPixelColor(0, strip.Color(0,0,255));
   strip.show();
 
   //get persistent values
-  pulseCount = EEPROM.read(EEPROM_ADDR_PULSE_COUNT);
-  maxPulseCount = EEPROM.read(EEPROM_ADDR_MAX_PULSE_COUNT);
-  percentFull = EEPROM.read(EEPROM_ADDR_PERCENT_FULL);
+//   pulseCount = EEPROM.read(EEPROM_ADDR_PULSE_COUNT);
+//   maxPulseCount = EEPROM.read(EEPROM_ADDR_MAX_PULSE_COUNT);
+//   percentFull = EEPROM.read(EEPROM_ADDR_PERCENT_FULL);
 
   //pin modes
   pinMode(pin0, INPUT_PULLDOWN);
@@ -81,20 +85,25 @@ void setup()
   pinMode(pin6, INPUT_PULLDOWN);
 
   pinMode(ledPin, OUTPUT);
+  
+  //read any values set in the EEPROM
+  maxPulseCount = EEPROM.read(EEPROM_ADDR_MAX_PULSE_COUNT);
+  pulseCount = EEPROM.read(EEPROM_ADDR_PULSE_COUNT);
+  percentFull = EEPROM.read(EEPROM_ADDR_PERCENT_FULL);
 
   //cloud variables
-  Spark.variable(pin0Name, &pin0Value, INT);
-  Spark.variable(pin1Name, &pin1Value, INT);
-  Spark.variable(pin2Name, &pin2Value, INT);
-  Spark.variable(pin3Name, &pin3Value, INT);
-  Spark.variable(pin4Name, &pin4Value, INT);
-  Spark.variable(pin5Name, &pin5Value, INT);
+//   Spark.variable(pin0Name, &pin0Value, INT);
+//   Spark.variable(pin1Name, &pin1Value, INT);
+//   Spark.variable(pin2Name, &pin2Value, INT);
+//   Spark.variable(pin3Name, &pin3Value, INT);
+//   Spark.variable(pin4Name, &pin4Value, INT);
+//   Spark.variable(pin5Name, &pin5Value, INT);
   Spark.variable(pin6Name, &pin6Value, INT);
 
   //https://api.particle.io/v1/devices/<device_id>/<variable_name>?access_token=<access_token>
-  Spark.variable("instantPulseCount", &instantPulseCount, INT);
+  Spark.variable("maxPCount", &maxPulseCount, INT);
+  Spark.variable("instPCount", &instantPulseCount, INT);
   Spark.variable("pulseCount", &pulseCount, INT);
-  Spark.variable("maxPulseCount", &maxPulseCount, INT);
   Spark.variable("percentFull", &percentFull, INT);
 
   //cloud functions - handle a cloud POST(data)
@@ -102,14 +111,20 @@ void setup()
   // -d access_token=<access_token> \
   // -d "args=<command>,<data0>,<dataN>"
   Spark.function("post", handlePost);
+  strip.setPixelColor(0, strip.Color(255,0,255));
+  strip.show();
 }
 
 int handlePost(String postData)
 {
   String* args = stringSplit(postData, ',');
   String command = args[0];
+  Serial.print("postData:");
+  Serial.println(postData);
+  
   if(command.equals("reset"))
   { //reset data
+    Serial.println("reset pulse count");
     return resetPulseCount();
   }
   else if(command.equals("setCalibration"))
@@ -128,29 +143,29 @@ void loop()
 //   pinVal(pin4, &pin4Value, pin4Name);
 //   pinVal(pin5, &pin5Value, pin5Name);
   pinVal(pin6, &pin6Value, pin6Name);
+  
+  float percent = (float)pulseCount / (float)maxPulseCount;
+  percentFull = (int)(percent * 100.0f);
+  if(percentFull > 100)
+    percentFull = 100;
+  EEPROM.update(EEPROM_ADDR_PERCENT_FULL, percentFull);
 
-  if(instantPulseCount > 0)
+  if(flowing)
   {
-    animateTap(animationStep);
+    animateTap();
   }
   else
   {
+    if(pulseCount > maxPulseCount)
+      pulseCount = maxPulseCount;
     EEPROM.update(EEPROM_ADDR_PULSE_COUNT, pulseCount);
-    float percent = (float)pulseCount / (float)maxPulseCount;
-    percentFull = (int)(percent * 100.0f);
-    EEPROM.update(EEPROM_ADDR_PERCENT_FULL, percentFull);
     recalculateTapColor();
   }
 }
 
-int getPulseCount()
-{
-  return pulseCount;
-}
-
 int resetPulseCount()
 {
-  pulseCount = 0;
+  pulseCount = maxPulseCount;
   EEPROM.update(EEPROM_ADDR_PULSE_COUNT, pulseCount);
   return 1;
 }
@@ -178,7 +193,9 @@ void pinVal(int pin, int* pinValue, String pinName)
       *pinValue = 0;
       digitalWrite(ledPin, HIGH);
       instantPulseCount++;
-      pulseCount++;
+      pulseCount--;
+      if(pulseCount < 0) //never go below 0
+        pulseCount = 0;
     }
   }
   
@@ -187,6 +204,11 @@ void pinVal(int pin, int* pinValue, String pinName)
   {
       // setPixels(instantPulseCount);
       instantPulseCount = 0;
+      if(previousPulseCount != pulseCount)
+        flowing = true;
+      else
+        flowing = false;
+      previousPulseCount = pulseCount;
       previousSecond = currentSecond;
   }
 }
@@ -199,41 +221,48 @@ void analogVal(int pin, int* pinValue, String pinName)
 }
 
 void recalculateTapColor()
-{ //change the color of the tap based on percentFull
-  //as percentFull goes from 100 to 0, gradiate 
-  //from green (0, 255, 0)
-  //to red     (255, 0, 0)
-  //the middle (yellow) is (255, 255, 0)
-
-  float multiplier = (255.0f + 255.0f) / 100.0f;
-  int value = multiplier * percentFull;
-  int red;
-  int green;
-  if(value > 255)
-  { //add to the red
-    red = value - 255;
-    green = 255;
-  }
-  else
-  { //subtract from green
-    red = 255;
-    green = value;
-  }
-
+{ 
   for(int i=0; i<PIXEL_COUNT; i++)
   {
-    strip.setPixelColor(i, strip.Color(red, green, 0));
+    strip.setPixelColor(i, getTapColor());
   }
   strip.show();
 }
 
-void animateTap(int animationStep)
+int getTapColor()
+{
+    //change the color of the tap based on percentFull
+    //as percentFull goes from 100 to 0, gradiate 
+    //from green (0, 255, 0)
+    //to red     (255, 0, 0)
+    //the middle (yellow) is (255, 255, 0)
+    float multiplier = (255.0f + 255.0f) / 100.0f;
+    int value = multiplier * percentFull;
+    int red;
+    int green;
+    //   Serial.print("value:");
+    //   Serial.println(value);
+    if(value > 255)
+    { //add to the red
+        green = 255;
+        red = 255 - value;
+    }
+    else
+    { //subtract from green
+        green = value;
+        red = 255;
+    }
+    
+    return strip.Color(red, green, 0);
+}
+
+void animateTap()
 { //do the flow animation
   if(animationStep >= PIXEL_COUNT)
     animationStep = 0;
 
   setAllPixelsOff();
-  strip.setPixelColor(animationStep, strip.Color(random(255), random(255), random(255)));
+  strip.setPixelColor((PIXEL_COUNT - 1) - animationStep, getTapColor());
   strip.show();
   delay(100);
   animationStep++;
